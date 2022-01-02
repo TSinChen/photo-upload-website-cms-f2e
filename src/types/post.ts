bbd4706a07b7ff2484572a8973990cc2cfb50f98
TSin Chen <sinin60076@gmail.com>
2022-01-02 15:58:20 +0800

    post types

---
+++ src/types/post.ts
@@ -0,0 +1,2 @@
+export type Post = { _id: string; title: string; description: string; imageLink: string; createdAt: string }
+export type PostBody = { title: string; description: string; imageLink: string }
