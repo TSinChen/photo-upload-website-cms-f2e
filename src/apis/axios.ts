977859df88d1cffb309e6bd82e2bda69ff6f158e
TSin Chen <sinin60076@gmail.com>
2022-01-03 15:07:17 +0800

    test env

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,5 +1,3 @@
 import axios from 'axios'
 
-export default axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://photo-upload-website-cms-b2e.herokuapp.com/',
-})
+export default axios.create({ baseURL: process.env.REACT_APP_API_URL })
