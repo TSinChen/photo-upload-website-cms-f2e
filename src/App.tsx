b0d0432cba15933e3898cf22e6047806b09fe8fe
TSin Chen <sinin60076@gmail.com>
2022-01-03 15:02:42 +0800

    test env

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Form from './components/Form/Form'
 import List from './components/List/List'
 
 const App = () => {
-  console.log('test: ', process.env.API_URL)
   return (
     <Box sx={{ display: 'flex', margin: '40px auto', flexDirection: 'column', alignItems: 'center' }}>
       <Form />
